507ad554bf78aca99b0589c636d5b6904b100b7d
Ashish <Ashish.sharma@intigate.in>
2025-10-28 09:10:33 +0530

    temp branch

---
+++ example/android/app/src/main/kotlin/com/example/map_camera_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.map_camera_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/map_camera.dart
@@ -0,0 +1,3 @@
+library;
+
+export 'src/map_camera_location.dart';
